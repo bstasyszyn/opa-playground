0a3a668d52f4e74993909e2b29e53726033c0c8b
Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
2023-11-07 14:25:21 -0500

    Update license headers.
    Signed-off-by: Bob Stasyszyn <Bob.Stasyszyn@securekey.com>

--- pkg/evaluator/prcache.go
+++ pkg/evaluator/prcache.go
@@ -1,5 +1,5 @@
 /*
-   Copyright SecureKey Technologies Inc.
+   Copyright Gen Digital Inc.
 
    This file contains software code that is the intellectual property of SecureKey.
    SecureKey reserves all rights in the code and you may not use it without
